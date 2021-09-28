e8fb23bd4d1c8ffc5150cfaba0520e7d38e0d185
DavidMach2 <31442735+DavidMach2@users.noreply.github.com>
2021-09-27 23:51:07 -0300

    Update script.js

--- Mentalista/script.js
+++ Mentalista/script.js
@@ -1,30 +1,30 @@
 var numeroSecreto = parseInt(Math.random() * 11)
-var tentativa = 3
+var tentativa = 0
 
 function Chutar() {
   var elementoResultado = document.getElementById('resultado')
   var chute = parseInt(document.getElementById('valor').value)
   console.log(numeroSecreto)
 
-  if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = 'Acertô!'
-    tentativa = 3
-    numeroSecreto = parseInt(Math.random() * 11)
-  } else if (chute > 10 || chute < 0) {
+  if (chute > 10 || chute < 0) {
     elementoResultado.innerHTML = 'Digite um número entre 0 e 10!'
+  } else if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = 'Acertô!'
+    tentativa = 0
+    numeroSecreto = parseInt(Math.random() * 11)
   } else if (chute < numeroSecreto) {
     elementoResultado.innerHTML =
-      'Erroooou! O numero secreto é MAIOR! | Tentativa ' + tentativa + ' de 3'
-    tentativa--
+      'Erroooou! O numero secreto é MAIOR! | Tentativa ' + (tentativa + 1) + ' de 3'
+    tentativa++
   } else if (chute > numeroSecreto) {
     elementoResultado.innerHTML =
-      'Erroooou! O numero secreto é MENOR! | Tentativa ' + tentativa + ' de 3'
-    tentativa--
+      'Erroooou! O numero secreto é MENOR! | Tentativa ' + (tentativa + 1) + ' de 3'
+    tentativa++
   }
-  if (tentativa == 0) {
+  if (tentativa == 3) {
     elementoResultado.innerHTML =
       'Que pena, acabaram as chances | Jogo Resetado'
-    tentativa = 3
+    tentativa = 0
     numeroSecreto = parseInt(Math.random() * 11)
   }
 }
